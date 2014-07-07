0b8f8177b7f9bac96efd9c1a24a37fe31d820ffd
Howard Chan <guanhao3797@gmail.com>
2014-07-07 18:35:03 +0800

    Modularized functions
    This makes the function more portable around.
    
    It also avoids an extra call as it as at the
    global scope.

--- test/static/js/light.js
+++ test/static/js/light.js
@@ -26,55 +26,70 @@ var light = function(views) {
     routes = views.html;
     html_dict = {};
 
-    // If base_url is given, just concat it before every route
-    if ( views.hasOwnProperty('base_url')  ) {
-        for (var i = 0; i < routes.length; i++) {
-            routes[i] = views.base_url + '/' + routes[i];
-        }
-    }
-    
-    // Checks if browser supports localStorage
-    function support_storage() {
-        try {
-            return 'localStorage' in window &&
-            window['localStorage'] !== null;
-        }
-        catch(e) {
-            return false;
-        }
-    }
-
-    // Iterates through routes and make Ajax requests to them
-    // Then, store their HTML into html_dict object with the route as key
-    routes.forEach(function(route) {
-        $jq.ajax({
-            url: '/' + route,
-            async: false,
-            success: function(html) {
-                html_dict['/' + route] = html;
+    function store_html() {
+        // Checks if browser supports localStorage
+        function support_storage() {
+            try {
+                return 'localStorage' in window &&
+                window['localStorage'] !== null;
             }
-        });
-    });
-
-    // If supported, iterate through html_dict object
-    // And store it in localStorage
-    if ( support_storage() ) {
-        var route, html, compr_html;
-        var total_size = 0;
-        for (route in html_dict) {
-            html = html_dict[route];
-
-            // Compress HTML and insert localStorage
-            compr_html = LZString.compress(html);
-            var size = compr_html.length;
-            log(size);
-            total_size += size;
-            localStorage[route] = compr_html;
+            catch(e) {
+                return false;
+            }
         }
-        log(total_size);
+
+        // If base_url is given, just concat it before every route
+        if ( views.hasOwnProperty('base_url')  ) {
+            for (var i = 0; i < routes.length; i++) {
+                routes[i] = views.base_url + '/' + routes[i];
+            }
+        }
+        
+        // Iterates through routes and make Ajax requests to them
+        // Then, store their HTML into html_dict object with the route as key
+        routes.forEach(function(route) {
+            $jq.ajax({
+                url: '/' + route,
+                async: false,
+                success: function(html) {
+                    html_dict['/' + route] = html;
+                }
+            });
+        });
+
+        // If supported, iterate through html_dict object
+        // And store it in localStorage
+        if ( support_storage() ) {
+            var route, html, compr_html,
+                total_size, file_size;
+
+            total_size = 0;
+            for (route in html_dict) {
+                html = html_dict[route];
+
+                // Compress HTML and insert localStorage
+                compr_html = LZString.compress(html);
+
+                // Sums up total storage size
+                size = compr_html.length;
+                total_size += size;
+
+                // Assigns compressed html to route
+                localStorage[route] = compr_html;
+
+                // Creates tokens to make sure Light only runs once
+                var token = 'ran';
+                localStorage['light_token'] = token;
+            }
+        }
+        else {
+            throw new FeatureUnsupported("Browser does not support localStorage");
+        }
     }
-    else {
-        throw new FeatureUnsupported("Browser does not support localStorage");
+
+    // Checks if html is stored
+    if ( ! localStorage.hasOwnProperty('light_token')  ) {
+        store_html();
     }
 
     /* ================================
